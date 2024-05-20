afd61c5d8f13dabfa4850ecb8c33d9950011b1a3
Chaeyoon Song <cs3886@drexel.edu>
2024-05-20 10:37:32 -0400

    change version for compatibility

--- AppDemo/app/build.gradle.kts
+++ AppDemo/app/build.gradle.kts
@@ -45,9 +45,9 @@ dependencies {
     implementation("com.google.firebase:firebase-storage")
     implementation("com.google.firebase:firebase-firestore:24.11.1")
     implementation("androidx.activity:activity:1.8.0")
-    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
 }
