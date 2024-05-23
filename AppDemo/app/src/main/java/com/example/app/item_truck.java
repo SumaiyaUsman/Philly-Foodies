c7336ed57afce17070ac0ba543926877e2c242fc
Sumaiya Usman <sumaiyausman@n3-8-58.dhcp.drexel.edu>
2024-05-23 15:55:41 -0400

    connected menu page to food truck name in the food truck list page - sumaiya

--- AppDemo/app/src/main/java/com/example/app/item_truck.java
+++ AppDemo/app/src/main/java/com/example/app/item_truck.java
@@ -2,13 +2,29 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+// Author: Sumaiya Usman 5/23/24
 public class item_truck extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_truck);
-    }
-}
+
+        textView = findViewById(R.id.happystar);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), happystar_menu.class);
+                startActivity(intent);
+                finish();
+            }
+    });
+}}
