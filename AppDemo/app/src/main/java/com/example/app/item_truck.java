19acb776ab5d82374da280163830150ff27264eb
Sumaiya Usman <sumaiyausman@Sumaiyas-MacBook-Pro.local>
2024-05-25 23:25:59 -0400

    connected menus to foodtruck names

--- AppDemo/app/src/main/java/com/example/app/item_truck.java
+++ AppDemo/app/src/main/java/com/example/app/item_truck.java
@@ -10,16 +10,25 @@ import android.widget.TextView;
 // Author: Sumaiya Usman 5/23/24
 public class item_truck extends AppCompatActivity {
 
-    TextView textView;
+    TextView happyStar;
+
+    TextView wokWorks;
+
+    TextView halalTaste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_truck);
 
-        textView = findViewById(R.id.happystar);
+        happyStar = findViewById(R.id.happystar);
 
-        textView.setOnClickListener(new View.OnClickListener() {
+        wokWorks = findViewById(R.id.wokworks);
+
+        halalTaste = findViewById(R.id.halaltaste);
+
+        //Author: Sumaiya Usman 5/24/24
+        happyStar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), happystar_menu.class);
@@ -27,4 +36,25 @@ public class item_truck extends AppCompatActivity {
                 finish();
             }
         });
+
+        //Author: Sumaiya Usman 5/25/24
+        wokWorks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), wokworks_menu.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //Author: Sumaiya Usman 5/25/24
+        halalTaste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), halaltaste_menu.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }}
